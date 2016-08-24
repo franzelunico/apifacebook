706daf6416fc4aee9d73191225400afdbb6c6243
Franz Lopez <franzelunico@gmail.com>
2016-08-24 22:02:23 +0000

    admins static files

--- apifacebook/settings.py
+++ apifacebook/settings.py
@@ -119,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_DIR = os.path.abspath(os.path.dirname(BASE_DIR))
+PUBLIC_DIR = os.path.join(BASE_DIR, 'public')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
