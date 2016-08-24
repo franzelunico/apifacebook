049314a52c49422cf5807b44e457c0bfeb72180f
Franz Lopez <franzelunico@gmail.com>
2016-08-24 19:53:21 +0000

    base for producction

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-
+rm -rf gettoken/migrations/*
+find . -name "*.pyc" -exec rm -vrf {} \;
+rm -rf gettoken/migrations/*
+rm db.sqlite3
 python manage.py makemigrations
 python manage.py migrate
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'pass')" | python manage.py shell
+python manage.py makemigrations gettoken
+python manage.py migrate gettoken
 python manage.py runserver 0.0.0.0:8000
